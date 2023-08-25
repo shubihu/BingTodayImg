c6a8ab5a78b6863b2eddbbd5e5db8ffe3dee89bf
42 <36021958+shubihu@users.noreply.github.com>
2023-08-25 13:18:54 +0800

    Add files via upload

---
+++ bingTodayImg.py
@@ -0,0 +1,30 @@
+import re
+import json
+from datetime import datetime
+from urllib.parse import urljoin
+
+import requests
+
+url = 'https://cn.bing.com'
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+}
+
+res = requests.get(url, headers=headers)
+res.encoding = res.apparent_encoding
+
+ret = re.search("var _model =(\{.*?\});", res.text)
+if not ret:
+    return
+
+data = json.loads(ret.group(1))
+image_content = data['MediaContents'][0]['ImageContent']
+
+image_url = urljoin(url, image_content['Image']['Url'])
+r = requests.get(image_url)
+with open('today.png', 'wb') as f:
+    f.write(r.content)
+
+
+
